04e7a58619213d46e9c89e3f8ce32cd23de19b04
Jhesvany Bozo <hasperjan.jas@gmail.com>
2026-02-02 09:03:56 -0400

    subida de erro de pasido en seccion

--- app/Models/Seleccion.php
+++ app/Models/Seleccion.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use App\Models\pasivodos;
+use App\Models\Pasivodos;
 
 class Seleccion extends Model
 {
